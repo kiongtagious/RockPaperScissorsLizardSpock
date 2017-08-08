92cdc8eebe46cd9fdcbdfc98b4f6a83486dcca5e
kiongtagious <kionglao20@gmai.com>
2017-08-08 13:56:12 -0500

    First commit with Weather Class Completed

---
+++ LemonadeStand/LemonadeStand/weather.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class weather
+    {
+        public string getWeather;
+        public string currentWeather;
+        public int getTemperature;
+        public int currentTemperature;
+        public string tomorrowsWeather;
+        public int tomorrowsTemperature;
+        Random randomWeather = new Random();
+        public weather()
+        {
+            getWeather = SetWeather();
+            currentWeather = getWeather;
+            getTemperature = SetTemperature();
+            currentTemperature = getTemperature;
+            tomorrowsWeather = SetWeather();
+            tomorrowsTemperature = SetTemperature();
+        }
+        public string SetWeather()
+        {
+            int weather = randomWeather.Next(1, 4);
+            switch (weather)
+            {
+                case 1:
+                    getWeather = "Sunny Day";
+                    break;
+                case 2:
+                    getWeather = "Rainy Day";
+                    break;
+                case 3:
+                    getWeather = "Cloudy Day";
+                    break;
+            }
+            return getWeather;
+        }
+        public int SetTemperature()
+        {
+            Random randomTemperature = new Random();
+            getTemperature = randomTemperature.Next(45, 100);
+            return getTemperature;
+        }
+        public int ChangeWeather()
+        {
+            if (getWeather == "Rainy Day")
+            {
+                currentTemperature = currentTemperature - 10;
+                return currentTemperature;
+            }
+            else if (getWeather == "Cloudy Day")
+            {
+                currentTemperature = currentTemperature - 5;
+                return currentTemperature;
+            }
+            else
+            {
+                currentTemperature = currentTemperature + 5;
+                return currentTemperature;
+            }
+        }
+        public void SetCurrentWeather()
+        {
+            currentWeather = tomorrowsWeather;
+            currentTemperature = tomorrowsTemperature;
+            tomorrowsWeather = SetWeather();
+            tomorrowsTemperature = SetTemperature();
+        }
+    }
+}
